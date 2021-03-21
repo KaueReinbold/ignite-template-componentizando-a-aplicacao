a27a6b5aa54f432189eca0ca2f58bc06bb859162
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-03-20 21:08:27 -0300

    feat: sidebar component.

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,32 @@
+import './styles.scss';
+
+import { Button } from '../Button';
+import { useWatchMe } from '../../hooks/useWatchMe';
+
+export function SideBar() {
+  const { genres, selectedGenreId, setSelectedGenreId } = useWatchMe();
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
